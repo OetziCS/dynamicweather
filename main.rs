94943e815f42389098ab70c81c01eac2984118f0
Oetzi_CS <130790194+OetziCS@users.noreply.github.com>
2023-09-14 20:27:01 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,1 +1,24 @@
+#[macro_use]
+extern crate actix_web;
+
+use std::{env, io};
+
+use actix_web::{middleware, App, HttpServer};
+
+
+
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+        .wrap(middleware::Logger::default())
+           
+
+        })
+    .bind("0.0.0.0:9090")?
+    .run()
+    .await
+}
 
