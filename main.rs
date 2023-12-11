f690dcc3d192128a5edb404c0d812ab1e9ce20bf
Oetzi_CS <130790194+OetziCS@users.noreply.github.com>
2023-12-11 20:59:27 +0100

    Update main.rs to use the latest weather information from weather.rs script.

--- main.rs
+++ main.rs
@@ -2,10 +2,21 @@
 extern crate actix_web;
 
 use std::{env, io};
+use actix_web::{middleware, web, App, HttpServer, HttpResponse};
+use crate::weather::{get_current_weather, WeatherInfo};
 
-use actix_web::{middleware, App, HttpServer};
+mod weather;
 
-
+async fn index() -> HttpResponse {
+    if let Some(weather_info) = get_current_weather() {
+        HttpResponse::Ok().body(format!(
+            "Current Weather: Main - {}, Description - {}",
+            weather_info.main, weather_info.description
+        ))
+    } else {
+        HttpResponse::NotFound().body("Weather information not available yet.")
+    }
+}
 
 async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
@@ -13,9 +24,8 @@ async fn main() -> io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-        .wrap(middleware::Logger::default())
-           
-
+          .wrap(middleware::Logger::default())
+          .service(web::resource("/").route(web::get().to(index)))
         })
     .bind("0.0.0.0:9050")?
     .run()
