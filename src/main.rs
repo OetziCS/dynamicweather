5fe77bc0ca39382e94e5b0152b076748b9e69169
OetziCS <130790194+OetziCS@users.noreply.github.com>
2023-12-13 17:17:31 +0100

    Fixed some issues 3.0

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ async fn index() -> HttpResponse {
     }
 }
 
+#[tokio::main]
 async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
