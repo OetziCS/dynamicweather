b427095cdc4eba3271dc688388e3a51252a6aea3
OetziCS <130790194+OetziCS@users.noreply.github.com>
2023-12-14 20:04:28 +0100

    Fixed some Issues

--- src/weather.rs
+++ src/weather.rs
@@ -39,7 +39,12 @@ async fn get_weather(api_key: &str, lat: f64, lon: f64) -> Result<(), reqwest::E
 
     let response = reqwest::get(url).await?;
     let weather_data_1 = response.text().await?;
-    let weather_data: serde_json::Value = serde_json::from_str(&weather_data_1).map_err(|err| reqwest::Error::from(err))?;
+    
+    let weather_data: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(&weather_data_1);
+    let weather_data = match weather_data {
+        Ok(data) => data,
+        Err(err) => return Err(reqwest::Error::from(err)),
+    };
 
     // Extract weather information
     let weather_info = WeatherInfo {
